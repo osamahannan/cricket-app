1436565b9967d3f6b14435d32895aaa4350d1c25
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-11-19 16:39:29 +0530

    practised animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+
+  useEffect(() => {
+    Aos.init({ duration: 2000, once: true })
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <h1>Hello World 1</h1>
+      </div>
+      <div data-aos="fade-up" className="container">
+        <h1>Hello World 2</h1>
+      </div>
+      <div data-aos="fade-left" className="container">
+        <h1>Hello World 3</h1>
+      </div>
+      <div data-aos="fade-right" className="container">
+        <h1>Hello World 4</h1>
+      </div>
+      <div data-aos="fade-left" className="container">
+        <h1>Hello World 5</h1>
+      </div>
+      <div data-aos="fade-up" className="container">
+        <h1>Hello World 6</h1>
+      </div>
     </div>
   );
 }
