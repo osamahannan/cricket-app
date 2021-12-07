bda7d556b019f3c5edd22d3297f175b9ad27b296
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-12-07 11:58:42 +0530

    added player cards by mapping through json object

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,14 +1,7 @@
 import React, { useEffect } from 'react'
 import Aos from "aos";
 import "aos/dist/aos.css";
-import osamapic from "../Assets/Profile pics/osama.svg";
-import minhajpic from "../Assets/Profile pics/minhaj.svg";
-import faizpic from "../Assets/Profile pics/Faiz1.svg";
-import rashidpic from "../Assets/Profile pics/Rashid.svg";
-import intzarpic from "../Assets/Profile pics/Intzar.svg";
-import zainpic from "../Assets/Profile pics/Zain.svg";
-import irfanpic from "../Assets/Profile pics/Irfan1.svg";
-import dummypic from "../Assets/Profile pics/profile.svg";
+import { players } from "../Helper/players"
 
 const Team = () => {
 
@@ -22,126 +15,20 @@ const Team = () => {
                 <h1>Our Team</h1>
             </div>
             <div className="player-container">
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={osamapic} alt="Osama" />
+
+                {players.map((player) => {
+                    return (
+                        <div className="card-container" data-aos="flip-right" key={player.id}>
+                            <div className="player-card">
+                                <div className="profile">
+                                    <img src={player.url} alt="Profile Pic" />
+                                </div>
+                                <span className="player-card-name">{player.name}</span>
+                            </div>
                         </div>
-                        <span className="player-card-name">Osama Hannan</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={minhajpic} alt="Minhaj" />
-                        </div>
-                        <span className="player-card-name">Minhaj Ahmad Khan</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-left">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={faizpic} alt="Faiz" />
-                        </div>
-                        <span className="player-card-name">Mohammad Faiz</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={rashidpic} alt="Rashid" />
-                        </div>
-                        <span className="player-card-name">Rashid</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={intzarpic} alt="Intzar" />
-                        </div>
-                        <span className="player-card-name">Intzar Khan</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={zainpic} alt="Zain" />
-                        </div>
-                        <span className="player-card-name">Zain Zaman Khan</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={dummypic} alt="dummypic" />
-                        </div>
-                        <span className="player-card-name">Shahbaz Ahmad Khan</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-left">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={dummypic} alt="dummypic" />
-                        </div>
-                        <span className="player-card-name">Asad Raiyan</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={irfanpic} alt="Irfan" />
-                        </div>
-                        <span className="player-card-name">Irfan Khan</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={dummypic} alt="dummypic" />
-                        </div>
-                        <span className="player-card-name">Sheebu</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={dummypic} alt="dummypic" />
-                        </div>
-                        <span className="player-card-name">Imran Khan</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={dummypic} alt="dummypic" />
-                        </div>
-                        <span className="player-card-name">Shareef</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-left">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={dummypic} alt="dummypic" />
-                        </div>
-                        <span className="player-card-name">Atif Shaikh</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={dummypic} alt="dummypic" />
-                        </div>
-                        <span className="player-card-name">Sajid</span>
-                    </div>
-                </div>
-                <div className="card-container" data-aos="flip-right">
-                    <div className="player-card">
-                        <div className="profile">
-                            <img src={dummypic} alt="dummypic" />
-                        </div>
-                        <span className="player-card-name">Arif Bhai</span>
-                    </div>
-                </div>
+                    )
+                })}
+
             </div>
         </div >
 
---
+++ src/Helper/players.js
@@ -0,0 +1,89 @@
+import osamapic from "../Assets/Profile pics/Osama.svg";
+import minhajpic from "../Assets/Profile pics/Minhaj.svg";
+import faizpic from "../Assets/Profile pics/Faiz.svg";
+import rashidpic from "../Assets/Profile pics/Rashid.svg";
+import intzarpic from "../Assets/Profile pics/Intzar.svg";
+import zainpic from "../Assets/Profile pics/Zain.svg";
+import irfanpic from "../Assets/Profile pics/Irfan.svg";
+import dummypic from "../Assets/Profile pics/profile.svg";
+
+export const players = [
+
+    {
+        id: 1,
+        name : "Minhaj Ahmad Khan",
+        url : minhajpic
+    },
+    {
+        id: 2,
+        name : "Zain Zaman Khan",
+        url : zainpic
+    },
+    {
+        id: 3,
+        name : "Osama Hannan",
+        url : osamapic
+    },
+    {
+        id: 4,
+        name : "Mohammad Faiz",
+        url : faizpic
+    },
+    {
+        id: 5,
+        name : "Asad Raiyan",
+        url : dummypic
+    },
+    {
+        id: 6,
+        name : "Syed Faraz",
+        url : dummypic
+    },
+    {
+        id: 7,
+        name : "Rashid",
+        url : rashidpic
+    },
+    {
+        id: 8,
+        name : "Atif Shaikh",
+        url : dummypic
+    },
+    {
+        id: 9,
+        name : "Shahbaz Ahmad Khan",
+        url : dummypic
+    },
+    {
+        id: 10,
+        name : "Irfan Khan",
+        url : irfanpic
+    },
+    {
+        id: 11,
+        name : "Intzar",
+        url : intzarpic
+    },
+    {
+        id: 12,
+        name : "Arif Bhai",
+        url : dummypic
+    },
+    {
+        id: 13,
+        name : "Imran Khan",
+        url : dummypic
+    },
+    {
+        id: 14,
+        name : "Shareef",
+        url : dummypic
+    },
+    {
+        id: 15,
+        name : "Sheebu",
+        url : dummypic
+    },
+
+
+]
