17b4d105a3caee81b9fa9b1c85613187ecf8113f
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-12-08 09:47:13 +0530

    added hamburger manu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import logopic from "../Assets/Annihilators3.png";
+import { useMediaQuery } from 'react-responsive'
+import { ImCross } from "react-icons/im";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+
+    const isMobile = useMediaQuery({ query: "(max-width: 750px)" });
+    const [showMenu, setShowMenu] = useState(false);
+    const [showLink, setShowLink] = useState(1);
+
+    const scrollToBottom = () => {
+        window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: 'smooth'
+        });
+    };
+
     return (
         <nav className="navbar">
             <div className="logo">
@@ -10,11 +25,24 @@ const Navbar = () => {
                     <img src={logopic} alt="Annihilators" />
                 </Link>
             </div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/team">Our Team</Link></li>
-                <li><Link to="/funds">Our Funds</Link></li>
-                <li><Link to="#/">Contact</Link></li>
+            {isMobile ? (<ul>
+                {showMenu ? <ImCross className="cross" onClick={() => setShowMenu(!showMenu)} /> : <GiHamburgerMenu className="hamburger" onClick={() => setShowMenu(!showMenu)} />} </ul>
+            ) : ""}
+
+            <ul className={showMenu ? "hidden active" : "hidden"}>
+                <div className="navdiv">
+                    <li>
+                        <Link to="/" onClick={() => { setShowMenu(false); setShowLink(1); }} className={(showLink === 1) ? "noSelect active-link" : "noSelect"}> Home </Link>
+                    </li>
+                    <li>
+                        <Link to="/team" onClick={() => { setShowMenu(false); setShowLink(2); }} className={(showLink === 2) ? "noSelect active-link" : "noSelect"}> Our Team </Link>
+                    </li>
+                    <li>
+                        <Link to="/funds" onClick={() => { setShowMenu(false); setShowLink(3); }} className={(showLink === 3) ? "noSelect active-link" : "noSelect"}> Our Funds </Link>
+                    </li>
+                    <li onClick={() => { setShowMenu(false); scrollToBottom(); }} className="noSelect li-contact"> Contact
+                    </li>
+                </div>
             </ul>
         </nav>
     )
