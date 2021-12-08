f8e1e8b97e7fc74bc5bb511a33bd488e069b65b2
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-12-08 18:09:31 +0530

    updated profile pic in team section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -85,10 +85,10 @@ const Home = () => {
 
             <div className="about-section">
                 <div className="about-container" data-aos="fade-up">
-                    <div className="image_section">
+                    <div className="image_section" data-aos="fade-up">
                         <img src={cricketbg} alt="stadium background" />
                     </div>
-                    <div className="about">
+                    <div className="about" data-aos="fade-up">
                         <h1>About Us</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
