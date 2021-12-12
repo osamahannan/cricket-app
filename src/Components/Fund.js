c8ae90a5d32d1000988ea9e6e397981ca62295f6
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-12-12 21:27:09 +0530

    updated fund page

--- src/Components/Fund.js
+++ src/Components/Fund.js
@@ -5,28 +5,28 @@ const Fund = () => {
     return (
         <div className="parent-fund">
             <Fade up><div className="fund">
-                <h1>This is fund page</h1>
+                <h3>This is fund page</h3>
             </div></Fade>
             <Fade left><div className="fund">
-                <h1>This is fund page</h1>
+                <h3>This is fund page</h3>
             </div></Fade>
             <Fade right><div className="fund">
-                <h1>This is fund page</h1>
+                <h3>This is fund page</h3>
             </div></Fade>
             <Fade down><div className="fund">
-                <h1>This is fund page</h1>
+                <h3>This is fund page</h3>
             </div></Fade>
             <Fade up><div className="fund">
-                <h1>This is fund page</h1>
+                <h3>This is fund page</h3>
             </div></Fade>
             <Fade left><div className="fund">
-                <h1>This is fund page</h1>
+                <h3>This is fund page</h3>
             </div></Fade>
             <Fade down><div className="fund">
-                <h1>This is fund page</h1>
+                <h3>This is fund page</h3>
             </div></Fade>
             <Fade right><div className="fund">
-                <h1>This is fund page</h1>
+                <h3>This is fund page</h3>
             </div></Fade>
         </div>
     )
