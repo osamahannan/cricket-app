6e57a941b2e3cf852dda95cb812de4e09df01c4e
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-12-19 20:47:43 +0530

    added icons for speciality of a player

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -3,7 +3,9 @@ import Fade from 'react-reveal/Fade';
 import Flip from 'react-reveal/Flip';
 import { Players } from "./Players"
 import Modal from "./Modal";
-
+import { GiTennisBall } from "react-icons/gi";
+import bat from "../backgrounds/bat.png";
+import batball from "../backgrounds/batball.png";
 
 const Team = ({ showModal, setShowModal }) => {
 
@@ -25,6 +27,7 @@ const Team = ({ showModal, setShowModal }) => {
                                         <div className="profile">
                                             <img src={player.image} alt="Profile Pic" />
                                         </div>
+                                        {(player.speciality === 'Bowler') ? <GiTennisBall className='ball' /> : (player.speciality === 'Batsman' ? <img src={bat} alt="Batsman" className='bat' /> : <img src={batball} alt="All Rounder" className='batball' />)}
                                         <span className="player-card-name">{player.name}</span>
                                     </div>
                                 </div></Flip>
