159ecd33fa5f4f3fccf2da77c1ee8a0e44b85eeb
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-12-21 14:05:52 +0530

    removed errors from fund page

--- src/Components/Fund Section/Fund.js
+++ src/Components/Fund Section/Fund.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { GiCoinflip } from "react-icons/gi";
 import { BsCalendar2Date } from "react-icons/bs";
 import { MdSportsCricket } from "react-icons/md";
